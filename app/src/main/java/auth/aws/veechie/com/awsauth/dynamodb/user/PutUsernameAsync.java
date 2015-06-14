6ce4d460c10fbc8ac80b8df5b0d6d520db23599c
Michael Brennan <Michael Brennan>
2015-06-14 16:11:59 -0600

    halfway through update username, need a break, unstable commit

--- app/src/main/java/auth/aws/veechie/com/awsauth/dynamodb/user/PutUsernameAsync.java
+++ app/src/main/java/auth/aws/veechie/com/awsauth/dynamodb/user/PutUsernameAsync.java
@@ -1,6 +1,16 @@
 package auth.aws.veechie.com.awsauth.dynamodb.user;
 
 import android.os.AsyncTask;
+
+import com.amazonaws.services.dynamodbv2.model.AttributeAction;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.AttributeValueUpdate;
+import com.amazonaws.services.dynamodbv2.model.ConditionalCheckFailedException;
+import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
+import com.amazonaws.services.dynamodbv2.model.UpdateItemRequest;
+
+import java.util.HashMap;
+
 import auth.aws.veechie.com.awsauth.model.User;
 import auth.aws.veechie.com.awsauth.utils.callback.PutUsernameCallback;
 
@@ -25,6 +35,29 @@ public class PutUsernameAsync extends AsyncTask<User, Void, Boolean> {
          */
 //        PutItemRequest putItemRequest = new PutItemRequest().withTableName("User")
 //                .withItem
+        try{
+            HashMap<String, AttributeValue> primaryKey = new HashMap<>();
+            AttributeValue email = new AttributeValue()
+                    .withS(users[0].getEmail());
+            primaryKey.put("EMAIL", email);
+
+            UpdateItemRequest request = new UpdateItemRequest()
+                    .withTableName("USERS")
+                    .withKey(primaryKey)
+                    .addAttributeUpdatesEntry(
+                            "USERNAME", new AttributeValueUpdate()
+                                        .withValue(new AttributeValue().withN(mPotentialUsername))
+                                        .withAction(AttributeAction.PUT))
+                    .addExpectedEntry(
+                            "USERNAME", new ExpectedAttributeValue()
+                                        .withValue()
+                    )
+
+                    )
+
+        }catch(ConditionalCheckFailedException e){
+
+        }
         return null;
     }
 
